e124d4698fee9773831ca4f2dd2ce1b7aa05b0bf
Christoph Seiler <christoph.seiler@gmail.com>
2020-09-04 23:44:40 +0200

    chore: add id to offer.

--- Backend.Models/Offer.cs
+++ Backend.Models/Offer.cs
@@ -5,6 +5,8 @@ namespace Backend.Models
 {
     class Offer
     {
+        public Guid Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
