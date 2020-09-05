c91cc63a2423b7c6c2a285fb501f6fc98ad782f0
Christoph Seiler <christoph.seiler@gmail.com>
2020-09-05 21:10:41 +0200

    chore: update data for guides and offers.

--- Backend.Core/Features/Guiding/Data/GuidesStartupTask.cs
+++ Backend.Core/Features/Guiding/Data/GuidesStartupTask.cs
@@ -36,7 +36,7 @@ namespace Backend.Core.Features.Guiding.Data
                     Id = Guid.Parse("5d017691-a2b7-4af8-b14e-357879746dc6"),
                     Firstname = "Hans",
                     Lastname = "Meier",
-                    Description = "Cooler Fischer!",
+                    Description = "Ich bin Hans, ich liebe es den Sommer in Bern zu verbringen. Am liebsten mit einem Eis auf dem See!",
                     Email = "hans.meier@test.ch",
                     Languages = new Collection<string>
                     {
@@ -47,9 +47,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("fae2f93f-00fa-44d8-ad73-b2162128b6ee"),
                     Firstname = "Reto",
                     Lastname = "Jost",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Lerne mit mir Bern kennen! Lebe seit über 20 Jahren in Bern und kenne die geheimen Ecken der Stadt.",
                     Email = "reto.jost@test.ch",
                     Languages = new Collection<string>
                     {
@@ -61,9 +62,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("b1ee7b15-1976-4152-ba15-e13f0e70e699"),
                     Firstname = "Veronika",
                     Lastname = "Haenni",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Verliebt in Bern - Hier wo man schnell in der Natur ist, komm mit mir in die Blumenfelder und entdecke den Kanton Bern von einer anderen Seite.",
                     Email = "veronika.haenni@test.ch",
                     Languages = new Collection<string>
                     {
@@ -75,9 +77,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("6a74f3f1-40dc-4590-9f75-3c6f59581d34"),
                     Firstname = "Mario",
                     Lastname = "Scholz",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Bern ist eine der Universitätsstädte der Schweiz und hat viel Kultur und Wissen zu bieten!",
                     Email = "mario.scholz@test.ch",
                     Languages = new Collection<string>
                     {
@@ -89,9 +92,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("9b5beddd-2632-4956-bbe3-702bef8ef5ae"),
                     Firstname = "Mike",
                     Lastname = "Schroeder",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Seit Jahren in der Schweiz und sehr vetraut mit den Traditionen, lass mir dir den Charm der Schweiz zeigen.",
                     Email = "mike.schroeder@test.ch",
                     Languages = new Collection<string>
                     {
@@ -103,9 +107,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("cf7a81fd-d556-4003-977b-7de77b38fa3f"),
                     Firstname = "Olf",
                     Lastname = "Dagobert",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Voll fett alteeer!",
                     Email = "olf.dagobert@test.ch",
                     Languages = new Collection<string>
                     {
@@ -117,9 +122,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("8baeb1b5-8b8d-42c2-86ae-416b6fbc9e8c"),
                     Firstname = "Anna",
                     Lastname = "Lehmann",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Schweiz ist der Angelpunkt der Kultur! Soviele Länder treffen sich hier. Ich freue mich dir meine Kultur zu zeigen und deine kennen zu lernen.",
                     Email = "anna.lehmann@test.ch",
                     Languages = new Collection<string>
                     {
@@ -131,9 +137,10 @@ namespace Backend.Core.Features.Guiding.Data
                 },
                 new User
                 {
+                    Id = Guid.Parse("bc851f39-92b7-4ab5-8b0c-6412e433281a"),
                     Firstname = "Ursula",
                     Lastname = "Holzmann",
-                    Description = "Voll der Botscha spieler",
+                    Description = "Wandern in der Schweiz ist das tollste im Leben!",
                     Email = "ursula.holzmann@test.ch",
                     Languages = new Collection<string>
                     {
