2c821e5dbe0fcfc602897a38d4c50052a30583de
Marc Sallin <marc.sallin@outlook.com>
2020-09-05 10:43:19 +0200

    test: add guides and usermanagement tests

---
+++ Backend.Tests/Integration/UsersControllerFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Backend.Core.Features.UserManagement.Requests;
+using Backend.Core.Features.UserManagement.Responses;
+using Backend.Tests.Utilities;
+using Xunit;
+
+namespace Backend.Tests.Integration
+{
+    [Trait("Category", "Integration")]
+    public class UsersControllerFixture : IClassFixture<TestContext>
+    {
+        private readonly TestContext _context;
+
+        public UsersControllerFixture(TestContext context) => _context = context;
+
+        [Fact]
+        public async Task Register_Successful()
+        {
+            string email = "marc.sallin@outlook.com";
+            var url = new Uri("api/users/Register", UriKind.Relative);
+            StringContent content = new RegisterUserRequest { Email = email, Password = "test" }.ToStringContent();
+            HttpResponseMessage response = await _context.AnonymousHttpClient.PostAsync(url, content);
+            UserLoginResponse userLoginResponse = await response.OnSuccessDeserialize<UserLoginResponse>();
+        }
+
+        [Fact]
+        public async Task SignIn_Successful()
+        {
+            string email = "t1@test.ch";
+            string password = "test";
+            var url = new Uri("api/users/Login", UriKind.Relative);
+            StringContent content = new UserLoginRequest { Email = email, Password = password }.ToStringContent();
+            HttpResponseMessage responseWithJwt = await _context.AnonymousHttpClient.PostAsync(url, content);
+            responseWithJwt.EnsureSuccessStatusCode();
+        }
+    }
+}
