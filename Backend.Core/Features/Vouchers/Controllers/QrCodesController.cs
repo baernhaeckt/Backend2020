3db4ba73c994b2220d98b07c684c43027b96d35d
Marc Sallin <marc.sallin@outlook.com>
2020-09-05 11:29:24 +0200

    feat: generate qr code

---
+++ Backend.Core/Features/Vouchers/Controllers/QrCodesController.cs
@@ -0,0 +1,28 @@
+﻿using Backend.Infrastructure.Abstraction.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace Backend.Core.Features.Vouchers.Controllers
+{
+    [Route("api/qrcode")]
+    [ApiController]
+    public class QrCodesController : ControllerBase
+    {
+        private readonly IWriter _writer;
+
+        public QrCodesController(IWriter writer)
+        {
+            _writer = writer;
+        }
+
+        [HttpPost]
+        public IActionResult Generate(string input)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q);
+            SvgQRCode qrCode = new SvgQRCode(qrCodeData);
+            string qrCodeAsSvg = qrCode.GetGraphic(20);
+            return Ok(qrCodeAsSvg);
+        }
+    }
+}
